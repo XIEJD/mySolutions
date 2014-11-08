7754d94f9539bc1ccf74a32ce5b5de2181a86617
xiejd <xiejidong888@qq.com>
2014-11-08 11:59:00 +0800

    修改格式

--- Classic_Algorithms/mergeSort.c
+++ Classic_Algorithms/mergeSort.c
@@ -7,20 +7,25 @@ void mergeSort(int a[], int length);
 void merge(int a[], int h, int m, int t);
 
 int main(){
-	int a[] = {9,8,7,6,5,4,3,2,1};
-	mergeSort(a,9);
-	for(int i=0; i<9; i++) printf("%d ",a[i]);
+	int a[] = {9,8,7,6,5,4,3,2,0,1};
+	mergeSort(a,10);
+	for(int i=0; i<10; i++){
+		 printf("%d ",a[i]);
+	}
 	return 0;
 }
 
 void mergeSort(int a[],int length){
 	int d = 2,z,y,m;
-	for(;d/2<length;d*=2){
+	for(; d/2<length; d*=2){
 		z = length/d;
 		y = length%d;
 		for(int i=0,h=0; i<=z; i++,h=i*d){
-			if(i<z) merge(a,h,h+d/2,h+d-1);
-			else if(y>d/2) merge(a,h,h+d/2,h+y-1);
+			if(i<z){
+				 merge(a,h,h+d/2,h+d-1);
+			}else if(y>d/2){
+				 merge(a,h,h+d/2,h+y-1);
+			}
 		}
 	}
 }
@@ -36,7 +41,11 @@ void merge(int a[], int h, int m, int t){
 			j++;
 		}
 	}
-	if(j<m) for(;j<m;i++,j++) tmp[i] = a[j];
-	if(k<=t) for(;k<=t;i++,k++) tmp[i] = a[k];
+	if(j<m){
+		 for(; j<m; i++,j++) tmp[i] = a[j];
+	}
+	if(k<=t){
+		for(; k<=t; i++,k++) tmp[i] = a[k];
+	}
 	for(int j = h,i = 0; i<t-h+1; i++,j++) a[j] = tmp[i];
 }
