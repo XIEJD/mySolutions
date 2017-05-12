02b6df2875aa59244f2715a34eaff37f90062fe1
XieJD <xjd@XieJDdeMacBook-Pro.local>
2017-05-12 19:53:02 +0800

    some script for automaticly solve some basic problems

---
+++ Environment_Deployment/init.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+echo 'Powered By XJD To ZhuangBi.'
+echo '!!! Make sure you have git. Or you should `sudo apt-get install git` first'
+echo 'Only for Ubuntu.'
+
+# download my vimrc file from my github repo.
+wget -P ~/ https://raw.githubusercontent.com/XIEJD/mySolutions/master/Environment_Deployment/.vimrc
+wget -P ~/ https://raw.githubusercontent.com/XIEJD/mySolutions/master/Environment_Deployment/.bash_profile
+
+# extract bundles name in .vimrc
+grep '^Plugin*' ~/.vimrc | grep -o \'.*\' | grep -o [a-zA-Z0-9\.\/\-]* > ~/.pathname
+
+# download bundles automaticly using git.
+for pathname in $(cat ~/.pathname)
+do 
+    repo=$(echo $pathname | grep -o '\/[a-zA-Z0-9\.\-]*$' | grep -o '[a-zA-Z0-9\.\-]*')
+    echo $repo
+    git clone --recursive https://github.com/${pathname}.git ~/.vim/bundle/$repo
+done
+
+# compile YCM
+echo 'Compiling...'
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --clang-completer
+
+echo 'Done.'
+
---
+++ Environment_Deployment/inst.sh
@@ -0,0 +1,73 @@
+#! /bin/bash
+
+echo 'Powered By XJD To ZhuangBi.'
+echo 'Only for Ubuntu.'
+
+# settings
+echo 'Which python version do you want to install ? EXAMPLE: 3.5.2'
+echo -n $PS2
+read VERSION
+PYTHON=Python-$VERSION
+INSTALL_DIR=~/Applications/$PYTHON
+DOWNLOAD_URL=https://www.python.org/ftp/python/${VERSION}/${PYTHON}.tgz
+DOWNLOAD_DIR=~/Downloads
+
+echo Dowloading ${PYTHON} from $DOWNLOAD_URL saves to $DOWNLOAD_DIR
+echo Installing to $INSTALL_DIR
+
+# download
+if [ -e $DOWNLOAD_DIR]; then
+    echo 'Using exist files.'
+else
+    wget -P $DOWNLOAD_DIR $DOWNLOAD_URL
+fi
+
+# decompress
+cd $DOWNLOAD_DIR
+echo 'Decompressing...'
+tar -zxf ./${PYTHON}.tgz
+if [ -d $PYTHON ]; then
+    cd $PYTHON
+else
+    echo 'Failed to decompress'
+    exit
+fi
+
+# create working directory
+if [ -d $INSTALL_DIR ]; then
+    echo Existed directory. $INSTALL_DIR
+else
+    mkdir -p $INSTALL_DIR
+    if [ -d $INSTALL_DIR ]; then
+        echo Created directory. $INSTALL_DIR
+    else
+        echo 'Can not create' $INSTALL_DIR
+        exit
+    fi
+fi
+
+# configure
+echo 'Configuring...'
+./configure -q --prefix $INSTALL_DIR
+
+# make
+echo 'Compiling...'
+make && make install
+if [ $? -eq 0 ]; then
+    echo 'Good make.'
+else
+    echo 'Bad make.'
+    exit
+fi
+
+# environment
+echo 'Setting environments...'
+echo alias python=~/Applications/${PYTHON}/bin/python3 >> ~/.bashrc
+echo alias pip=~/Applications/${PYTHON}/bin/pip3 >> ~/.bashrc
+echo PYTHONPATH=~/Applications/${PYTHON}/bin >> ~/.bashrc
+
+# update environment
+source ~/.bashrc
+
+echo 'Done.'
+echo 'If fail to execute python, try `source ~/.bashrc` in your terminal.'
